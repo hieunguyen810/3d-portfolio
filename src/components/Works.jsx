91034d17d9f2f40b6291e398a1dc87a7d7738247
hieunguyen810 <trihieu08101999@gmail.com>
2023-11-05 14:26:40 +0700

    update logo

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -81,10 +81,7 @@ const Works = () => {
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          real-world examples of my work. 
         </motion.p>
       </div>
 
